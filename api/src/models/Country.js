d083233f115ccd19f7565dc91830a795acb79de7
Fernando <lenin_jaramillo@hotmail.com>
2021-08-02 16:31:58 +0200

    model countries

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -8,5 +8,30 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    ID: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    imagenFlag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.STRING,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+    }
   });
 };
