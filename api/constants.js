bafcea5ec0c32750dca9430a51b8244d86565818
Fernando <lenin_jaramillo@hotmail.com>
2021-08-03 20:07:02 +0200

    connections

---
+++ api/constants.js
@@ -0,0 +1,9 @@
+const ALL_COUNTRIES = "https://restcountries.eu/rest/v2/all";
+const ONE_COUNTRY_BY_NAME = "https://restcountries.eu/rest/v2/name/{name}";
+const ONE_COUNTRY_BY_CODE = "https://restcountries.eu/rest/v2/alpha/{code}";
+
+module.esports = {
+    ALL_COUNTRIES,
+    ONE_COUNTRY_BY_NAME,
+    ONE_COUNTRY_BY_CODE,
+};
---
+++ api/src/models/Activity.js
@@ -0,0 +1,24 @@
+const {Sequelize, DataTypes } = require("sequelize"); 
+
+module.exports = (sequelize) => {
+    sequelize.define("activity", {
+        id: {
+            type: DataTypes.UUID, 
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+        },
+    //     name: {
+    //         type: DataTypes.STRING,
+    //     },
+    //     dificultyLevel: {
+    //         type: DataTypes.INTEGER,
+    //     },
+    //     term: {
+    //         type: DataTypes.STRING,
+
+    //     },
+    //     season: {
+    //         type: DataTypes.STRING,
+    //     },
+    });
+};
