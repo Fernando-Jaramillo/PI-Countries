5ebc32f5f022ff7475a7f6fed20508cb3ad703f6
Fernando <lenin_jaramillo@hotmail.com>
2021-08-12 21:39:43 +0200

    filter structure

---
+++ client/src/components/Filters/Filters.jsx
@@ -0,0 +1,4 @@
+import FContinent from "../FilterByContinent/FilterByContinent";
+import FActivity from "../FilterByActivity/FilterByActivity";
+import FAlphabet from "../FilterByAlphabet/FilterByAlphabet";
+import FPopulation from "../FilterByPopulation/FilterByPopulation";
